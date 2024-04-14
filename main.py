79f95168f913070d349fd21a2ef01de375acff77
solaire-astora <solaire.astora47@gmail.com>
2024-04-14 23:39:11 +0800

    111

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from urllib.parse import quote
 from bs4 import BeautifulSoup
 import codecs
 
+
 def to_unicode_escape(s):
     return codecs.unicode_escape_encode(s)[0].decode().replace('\\u', '%u')
 
@@ -26,11 +27,38 @@ def ask_url(url):
             print(e.reason)
     return html
 
+
 def parse_sight(html):
     soup = BeautifulSoup(html, "lxml")
+    divs = soup.find_all('div', class_='list_mod2')
+    for div in divs:
+        leftimg_div = div.find('div', class_='leftimg')
+        if not leftimg_div:
+            continue
+        a_tag = leftimg_div.find('a')
+        href = a_tag['href']
+
+        print(href)
 
 
+with open("html.txt", 'r', encoding='utf-8') as file:
+    parse_sight(file.read())
 
-print(quote("鼓浪屿"))
+#print(quote("鼓浪屿"))
 
-//print(ask_url("https://you.ctrip.com/sight/Suzhou11.html?keywords=%u9F13%u6D6A%u5C7F"))
+#print(ask_url("https://you.ctrip.com/sight/Suzhou11.html?keywords=%u9F13%u6D6A%u5C7F"))
+
+import re
+
+# 假设 href 是您要判断的链接
+href = 'https://you.ctrip.com/sight/suzhou11/57405.html'
+
+# 使用正则表达式进行匹配
+pattern = r'sight.*suzhou11'
+match = re.search(pattern, href)
+
+# 判断匹配结果
+if match:
+    print("Matched!")
+else:
+    print("Not matched.")
